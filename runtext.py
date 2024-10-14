7ce2ca71f2544e16fa8a2a56ccbf07c4d59a3ae9
Jonas <jonaskaz2009@outlook.de>
2024-10-14 14:02:39 +0200

    as

--- runtext.py
+++ runtext.py
@@ -6,27 +6,9 @@ import time
 
 def run(text):
     options = RGBMatrixOptions()
-
-    options.hardware_mapping = "adafruit-hat"
     options.rows = 32
     options.cols = 64
-    options.chain_length = 1
-    options.parallel = 1
-    options.row_address_type = 0
-    options.multiplexing = 0
-    options.pwm_bits = 11
-    options.brightness = 80
-    options.pwm_lsb_nanoseconds = 130
-    options.led_rgb_sequence = "RGB"
-    options.pixel_mapper_config = ""
-    options.panel_type = ""
-
-    # options.show_refresh_rate = 1
-    # options.gpio_slowdown = 1
-    # options.disable_hardware_pulsing = True
-    # options.drop_privileges=False
-
-    matrix = RGBMatrix(options = options)
+    matrix = RGBMatrix(options=options)
     offscreen_canvas = matrix.CreateFrameCanvas()
     font = graphics.Font()
     font.LoadFont("./fonts/7x13.bdf")
