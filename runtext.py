f9f723ecd9124d6dee9e4d4673108de54f157205
Jonas <jonaskaz2009@outlook.de>
2024-10-14 13:57:58 +0200

    merged samplebase in runtext

--- runtext.py
+++ runtext.py
@@ -1,36 +1,45 @@
 #!/usr/bin/env python
 # Display a runtext with double-buffering.
-from samplebase import SampleBase
-from rgbmatrix import graphics
+from rgbmatrix import RGBMatrix, RGBMatrixOptions, graphics
 import time
 
 
-class RunText(SampleBase):
-    def __init__(self, *args, **kwargs):
-        super(RunText, self).__init__(*args, **kwargs)
-        self.parser.add_argument("-t", "--text", help="The text to scroll on the RGB LED panel", default="Hello world!")
+def run(self):
+    options = RGBMatrixOptions()
 
-    def run(self):
-        offscreen_canvas = self.matrix.CreateFrameCanvas()
-        font = graphics.Font()
-        font.LoadFont("./fonts/7x13.bdf")
-        textColor = graphics.Color(255, 255, 0)
-        pos = offscreen_canvas.width
-        my_text = self.args.text
+    options.hardware_mapping = "adafruit-hat"
+    options.rows = 32
+    options.cols = 64
+    options.chain_length = 1
+    options.parallel = 1
+    options.row_address_type = 0
+    options.multiplexing = 0
+    options.pwm_bits = 11
+    options.brightness = 80
+    options.pwm_lsb_nanoseconds = 130
+    options.led_rgb_sequence = "RGB"
+    options.pixel_mapper_config = ""
+    options.panel_type = ""
 
-        while True:
-            offscreen_canvas.Clear()
-            len = graphics.DrawText(offscreen_canvas, font, pos, 10, textColor, my_text)
-            pos -= 1
-            if (pos + len < 0):
-                pos = offscreen_canvas.width
+    # options.show_refresh_rate = 1
+    # options.gpio_slowdown = 1
+    # options.disable_hardware_pulsing = True
+    # options.drop_privileges=False
 
-            time.sleep(0.05)
-            offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
+    matrix = RGBMatrix(options = options)
+    offscreen_canvas = matrix.CreateFrameCanvas()
+    font = graphics.Font()
+    font.LoadFont("./fonts/7x13.bdf")
+    textColor = graphics.Color(255, 255, 0)
+    pos = offscreen_canvas.width
+    my_text = self.args.text
 
+    while True:
+        offscreen_canvas.Clear()
+        len = graphics.DrawText(offscreen_canvas, font, pos, 10, textColor, my_text)
+        pos -= 1
+        if (pos + len < 0):
+            pos = offscreen_canvas.width
 
-# Main function
-if __name__ == "__main__":
-    run_text = RunText()
-    if (not run_text.process()):
-        run_text.print_help()
+        time.sleep(0.05)
+        offscreen_canvas = matrix.SwapOnVSync(offscreen_canvas)
